d5ad5f7edcb8e46a4bbc2a00d2b90f08703c2938
chinmay7g <gherde007@gmail.com>
2022-10-25 16:24:18 -0700

    Fixed bugs

--- init.sql
+++ init.sql
@@ -1,6 +1,9 @@
+DROP DATABASE IF EXISTS dsci551_project;
+CREATE DATABASE dsci551_project; 
+
 /* Writing table */
 
-CREATE TABLE Namenode
+CREATE TABLE IF NOT EXISTS Namenode
 ( 
   inode_num varchar(36),
   type char(1) not null,
@@ -11,9 +14,9 @@ CREATE TABLE Namenode
   ctime timestamp not null,
   permission smallint not null,
   primary key (inode_num)
-)
+);
 
-CREATE TABLE Datanode_1
+CREATE TABLE IF NOT EXISTS Datanode_1
 (
 data_block_id varchar(36),
 block_id varchar(36) not null,
@@ -22,9 +25,9 @@ primary key (data_block_id),
 foreign key (block_id) references Block_info_table(blk_id)
 on update cascade,
 on delete cascade
-)
+);
 
-CREATE TABLE Datanode_2
+CREATE TABLE IF NOT EXISTS Datanode_2
 (
 data_block_id varchar(36),
 block_id varchar(36) not null,
@@ -33,9 +36,9 @@ primary key (data_block_id),
 foreign key (block_id) references Block_info_table(blk_id)
 on update cascade,
 on delete cascade
-)
+);
 
-CREATE TABLE Datanode_3
+CREATE TABLE IF NOT EXISTS Datanode_3
 (
 data_block_id varchar(36),
 block_id varchar(36) not null,
@@ -44,9 +47,9 @@ primary key (data_block_id),
 foreign key (block_id) references Block_info_table(blk_id)
 on update cascade,
 on delete cascade
-)
+);
 
-CREATE TABLE Block_info_table
+CREATE TABLE IF NOT EXISTS Block_info_table
 (
 blk_id varchar(36),
 file_inode varchar(36) not null,
@@ -55,11 +58,11 @@ datanode_num smallint not null,
 offset smallint not null,
 primary key (blk_id),
 foreign key (file_inode) references Namenode(inode_num)
-)
+);
 
-CREATE TABLE Parent_Child
+CREATE TABLE IF NOT EXISTS Parent_Child
 (
 parent_inode varchar(36),
 child_inode varchar(36),
 primary key (child_inode)
-)
+);
