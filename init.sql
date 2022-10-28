cbc2188edc5bb307e18cf64e28560f6c23a0c7e6
Aman04jdsj <amanthir@usc.edu>
2022-10-27 18:46:21 -0700

    Added route for mkdir command. SQL changes to create root node.

--- init.sql
+++ init.sql
@@ -6,10 +6,10 @@ USE dsci551_project;
 
 CREATE TABLE IF NOT EXISTS Namenode
 ( 
-  inode_num VARCHAR(36),
+  inode_num BINARY(16),
   node_type CHAR(1) NOT NULL,
   name VARCHAR(10) NOT NULL,
-  replication INT NOT NULL,
+  replication INT,
   mtime TIMESTAMP,
   atime TIMESTAMP,
   ctime TIMESTAMP NOT NULL,
@@ -17,10 +17,12 @@ CREATE TABLE IF NOT EXISTS Namenode
   PRIMARY KEY (inode_num)
 );
 
+INSERT INTO Namenode VALUES (UNHEX(REPLACE(UUID(),'-','')), 'd', '/', NULL, NULL, NULL, NOW(), 755);
+
 CREATE TABLE IF NOT EXISTS Block_info_table
 (
-  blk_id VARCHAR(36),
-  file_inode VARCHAR(36) NOT NULL,
+  blk_id BINARY(16),
+  file_inode BINARY(16) NOT NULL,
   num_bytes INT NOT NULL,
   datanode_num smallint NOT NULL,
   offset smallint NOT NULL,
@@ -30,8 +32,8 @@ CREATE TABLE IF NOT EXISTS Block_info_table
 
 CREATE TABLE IF NOT EXISTS Datanode_1
 (
-  data_block_id VARCHAR(36),
-  block_id VARCHAR(36) NOT NULL,
+  data_block_id BINARY(16),
+  block_id BINARY(16) NOT NULL,
   content TEXT,
   PRIMARY KEY (data_block_id),
   FOREIGN KEY (block_id) REFERENCES Block_info_table(blk_id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -39,8 +41,8 @@ CREATE TABLE IF NOT EXISTS Datanode_1
 
 CREATE TABLE IF NOT EXISTS Datanode_2
 (
-  data_block_id VARCHAR(36),
-  block_id VARCHAR(36) NOT NULL,
+  data_block_id BINARY(16),
+  block_id BINARY(16) NOT NULL,
   content TEXT,
   PRIMARY KEY (data_block_id),
   FOREIGN KEY (block_id) REFERENCES Block_info_table(blk_id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -48,8 +50,8 @@ CREATE TABLE IF NOT EXISTS Datanode_2
 
 CREATE TABLE IF NOT EXISTS Datanode_3
 (
-  data_block_id VARCHAR(36),
-  block_id VARCHAR(36) NOT NULL,
+  data_block_id BINARY(16),
+  block_id BINARY(16) NOT NULL,
   content TEXT,
   PRIMARY KEY (data_block_id),
   FOREIGN KEY (block_id) REFERENCES Block_info_table(blk_id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -57,8 +59,8 @@ CREATE TABLE IF NOT EXISTS Datanode_3
 
 CREATE TABLE IF NOT EXISTS Parent_Child
 (
-  parent_inode VARCHAR(36),
-  child_inode VARCHAR(36),
+  parent_inode BINARY(16),
+  child_inode BINARY(16),
   PRIMARY KEY (child_inode),
   FOREIGN KEY (parent_inode) REFERENCES Namenode(inode_num) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY (child_inode) REFERENCES Namenode(inode_num) ON UPDATE CASCADE ON DELETE CASCADE
